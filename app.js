16100b598b0d301b3c6f624d374cbef9537e0b8c
jenith <kan077bct041@kec.edu.np>
2026-03-06 15:39:54 +0000

    added monitoring and logging

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const client = require("prom-client");
+
 const app = express();
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
 app.get("/", (req, res) => res.send("CI/CD demo running"));
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
+
 app.listen(3000, () => console.log("Listening on 3000"));
